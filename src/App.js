0398c1b79afd75bf0ee9d35bcb0be6b3e8e394ca
RITIK <ritikdhembla281@gmail.com>
2024-01-26 16:06:44 +0530

    Logo

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import Featuresdata from './components/Featuresdata';
 
 import Navbar from './components/Navbar';
 import Hero_section from './components/Hero_section';
+import Logo from './components/Logo';
 
 function App() {
   return (
     <>
   <Navbar/>
    <Hero_section/>
-{/* <Logo/> */}
+<Logo/>
 <Featuresdata/>
 
 
